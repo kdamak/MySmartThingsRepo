f7dd7b65d444a039d7b39eb2f21f1baf8542be87
constjs <constjs@users.noreply.github.com>
2015-09-26 17:19:15 -0400

    Update monoprice_motion_sensor.device.groovy

--- monoprice_motion_sensor.device.groovy
+++ monoprice_motion_sensor.device.groovy
@@ -24,7 +24,7 @@ preferences {
 }
 
 metadata {
-    definition (name:"My Monoprice Motion Sensor v2", namespace:"jscgs350", author: "florianz") {
+    definition (name:"My Monoprice Motion Sensor v2", namespace:"jscgs350", author: "several") {
         capability "Battery"
         capability "Motion Sensor"
         capability "Temperature Measurement"
